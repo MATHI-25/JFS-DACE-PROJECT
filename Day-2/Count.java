1d13a9c661b1e7fcc00508c528ca9471289c32fd
Srimathi <srimathia722@gmail.com>
2025-12-16 00:13:17 +0530

    Add

--- Day-2/Count.java
+++ Day-2/Count.java
@@ -1,5 +1,12 @@
-package JAVA-FS.Day-2;
-
-public class Count {
-    
+import java.lang.System;
+public class Count{
+    public static void main(String[] args) {
+        int n=1528;
+        int count=0;
+        for(int i=n;i>0;i=i/10)
+        {
+            count++;
+        }
+        System.out.println("Number of digits = " + count);
+    }
 }
